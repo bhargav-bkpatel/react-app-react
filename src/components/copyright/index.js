f7185067e22920c1a965e7f3502331a54b57f9be
Bhargav <33828401+bhargav-bkpatel@users.noreply.github.com>
2023-04-03 17:36:09 +0530

    Update

--- src/components/copyright/index.js
+++ src/components/copyright/index.js
@@ -9,50 +9,12 @@ const Copyright = (props) => {
         <span className="copyright-text">
           <span className="copyright-text1">
             © All rights reserved
-            <span
-              dangerouslySetInnerHTML={{
-                __html: ' ',
-              }}
-            />
+        
           </span>
-          <a
-            href="https://www.teleporthq.io"
-            target="_blank"
-            rel="noreferrer noopener"
-          >
-            <span className="copyright-text2">@TeleportHQ.</span>
-          </a>
-          <span className="copyright-text3">
-            {' '}
-            Powered by
-            <span
-              dangerouslySetInnerHTML={{
-                __html: ' ',
-              }}
-            />
-          </span>
-          <a
-            href="https://www.vercel.com"
-            target="_blank"
-            rel="noreferrer noopener"
-          >
-            <span className="copyright-text4">Vercel</span>
-          </a>
-          <span className="copyright-text5">
-            . Image source:
-            <span
-              dangerouslySetInnerHTML={{
-                __html: ' ',
-              }}
-            />
-          </span>
-          <a
-            href="https://www.unsplash.com"
-            target="_blank"
-            rel="noreferrer noopener"
-          >
-            <span className="copyright-text6">Unsplash</span>
-          </a>
+          
+            <span className="copyright-text2">@Bhargav</span>
+          
+        
           <span className="copyright-text7">.</span>
         </span>
       </div>
